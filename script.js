ac78753647ad77146b7b18c3e8ec082448e57b66
fernandogabriel04 <fernando.gabriel322@hotmail.com>
2022-06-03 16:43:03 -0300

    pag17

--- script.js
+++ script.js
@@ -1,33 +1,62 @@
 var canvas = document.getElementById("triangulo");
-var context = canvas.getContext('2d');
+var arte = canvas.getContext('2d');
     // implementando rect
     // quadrado principal azul
-context.fillStyle = "blue";
-context.fillRect(100, 50, 100, 100);
+arte.fillStyle = "blue";
+arte.fillRect(100, 50, 100, 100);
     // contorno esquerdo
-context.lineWidth = 10;
-context.strokeStyle = 'red';
-context.strokeRect(95, 45, 30, 30);
+arte.lineWidth = 10;
+arte.strokeStyle = 'red';
+arte.strokeRect(95, 45, 30, 30);
     // contorno direito
-context.lineWidth = 10;
-context.strokeStyle = 'red';
-context.strokeRect(160, 45, 30, 30);
+arte.lineWidth = 10;
+arte.strokeRect(160, 45, 30, 30);
     //contorno inferior
-context.lineWidth = 10;
-context.strokeStyle = 'red';
-context.strokeRect(120, 100, 45, 35);
+arte.lineWidth = 10;
+arte.strokeRect(120, 100, 45, 35);
     // implementando path
+arte.beginPath();
     // triangulo
-context.beginPath();
-context.fill();
-context.stroke();
-context.moveTo(150, 300);
-context.lineTo(250, 300);
-context.lineTo(150,200);
-context.lineTo(75,300);
-context.lineTo(150,300);
+arte.fill();
+arte.lineWidth = 2;
+arte.moveTo(150, 300);
+arte.lineTo(250, 300);
+arte.lineTo(150,200);
+arte.lineTo(75,300);
+arte.lineTo(150,300);
+arte.stroke();
 
-// context.moveTo(75, 300);
-// context.lineTo(250, 300);
-// context.lineTo(150,200);
-context.stroke();
+    // arco
+    // esquerdo
+arte.fillStyle = 'purple';
+arte.beginPath();
+arte.arc(300, 109, 40 , 90*Math.PI/180, 270*Math.PI/180, false);
+arte.strokeStyle = 'blue';
+arte.fill();
+arte.stroke();
+    // direito
+arte.fillStyle = 'purple';
+arte.beginPath();
+arte.arc(310, 109, 40 , 90*Math.PI/180, 270*Math.PI/180, true);
+arte.fill();
+arte.stroke();
+
+    // imagens
+
+var imagem = new Image();
+imagem.src = 'games-js-master/01/img/ovni.png'
+
+imagem.onload = function() {
+    //posição
+    var x = 35;
+    var y = 325;
+    //repetir a img
+    for (var i = 1; i <= 5; i++){
+        arte.drawImage(imagem,x,y,65,32);
+        x+=70;
+    }
+}
+    // reduzir img
+arte.drawImage(imagem, x, 400, 32, 16);
+    // ampliar img
+arte.drawImage(imagem, x, 400, 32, 16);
