9afc4c7ccbf5a9fd7ee10f32ccc4df40d25fdcb4
fernandogabriel04 <fernando.gabriel322@hotmail.com>
2022-06-07 17:46:24 -0300

    cap1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("triangulo");
+var canvas = document.getElementById("cap1");
 var arte = canvas.getContext('2d');
     // implementando rect
     // quadrado principal azul
@@ -55,8 +55,64 @@ imagem.onload = function() {
         arte.drawImage(imagem,x,y,65,32);
         x+=70;
     }
-}
     // reduzir img
-arte.drawImage(imagem, x, 400, 32, 16);
+    arte.drawImage(imagem, 50, 400, 32, 16);
     // ampliar img
-arte.drawImage(imagem, x, 400, 32, 16);
+    arte.drawImage(imagem, 200, 400, 128, 32);
+}
+
+            // pt2
+    //clipping
+var canvas2 = document.getElementById("cap1.2");
+var arte2 = canvas2.getContext("2d")
+var imagem2 = new Image();
+imagem2.src = 'games-js-master/01/img/explosao.png';
+imagem2.onload = function() {
+    arte2.drawImage(
+        imagem2,
+        80,10,60,65,    //área de recorte (clipping)
+        20,20,60,65     //desenho no canvas
+    )
+}
+    //save e restore
+arte2.fillStyle= 'red';
+arte2.fillRect(20,100,25,25);
+arte2.save();
+arte2.fillStyle= 'yellow';
+arte2.fillRect(70,100,25,25);
+arte2.restore();
+arte2.fillRect(120,100,25,25);
+
+    // animation pag 23 - pag 26 = nao entendi.
+//    var x = 30;
+//    var y = 200;
+//    var raio = 5;
+//var anterior = new Date().getTime();
+//requestAnimationFrame(levantarBola);
+//function levantarBola() {
+//    arte2.clearRect(0,0,canvas2.width, canvas2.height)  //apagar todo canvas anterior
+//    arte2.beginPath();
+//    arte2.arc(x,y,raio, 0, Math.PI*2);
+//    arte2.fill();
+//
+//    var agora = new Date().getTime();
+//    var anterior = agora;
+//    var decorrido = agora - anterior;
+//    var velocidade = 20;
+//    x += velocidade * decorrido / 1000;
+//
+//    //y-=50;
+// 
+//}
+//requestAnimationFrame(levantarBola)
+
+
+//carro
+
+function Carro(cor, velocidade){
+    this.cor = cor;
+    this.velocidade = velocidade;
+    this.velocidadeAtual = 0;
+}
+var meuCarro = new Carro('amarelo', 250);
+var oponente = new Carro('azul', 300);
